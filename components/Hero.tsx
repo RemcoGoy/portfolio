3da8693150db023f22daf8caa77df6cad1229c64
Remco Goyvaerts <remco.goyvaerts@mycellhub.com>
2022-08-31 17:29:11 +0200

    Responsive image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,7 +26,14 @@ const Hero: FunctionComponent<any> = () => {
       {/* Image container */}
       <div className="hidden lg:block relative w-full md:w-1/2 mx-auto -mr-40 mt-20">
         <div className="w-3/4">
-          <Image src={userData.avatarUrl} alt="avatar" height="500" width="500" className="shadow" />
+          <Image
+            src={userData.avatarUrl}
+            alt="avatar"
+            height="500"
+            width="500"
+            layout="responsive"
+            className="shadow"
+          />
           <div className="flex flex-row justify-between mt-4">
             <div className="flex flex-row space-x-4">
               <svg
