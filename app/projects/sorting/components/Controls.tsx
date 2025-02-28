ff6fc7ea56466cd25a3d2b0559d22c0f98e0bf5d
Remco Goyvaerts <remco.goyvaerts@acagroup.be>
2025-02-28 11:46:47 +0100

    Fix build

--- app/projects/sorting/components/Controls.tsx
+++ app/projects/sorting/components/Controls.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { useState } from "react";
-
 interface ControlsProps {
   arraySize: number;
   setArraySize: (size: number) => void;
--- app/projects/sorting/components/SortingVisualizer.tsx
+++ app/projects/sorting/components/SortingVisualizer.tsx
@@ -52,7 +52,7 @@ export default function SortingVisualizer() {
   useEffect(() => {
     generateArray();
     return () => resetTimeouts();
-  }, [arraySize]);
+  }, [generateArray, arraySize]);
 
   // Stop sorting
   const stopSorting = () => {
